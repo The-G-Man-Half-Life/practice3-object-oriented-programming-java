a5f6d9ff8cd7c4d968b019b58ddd0dfda53de1ff
4Lonus <lopezjpll06@gmail.com>
2025-11-08 19:30:45 -0500

    Implemented all needed Matrix Operations

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
 import utils.CVSReader;
+import utils.MatrixOperations;
 
 public class Main {
 	public static void main(String[] args) {
 		// First we need to read the values from the CVS given.
 		// We can stablish a new class that is only about this.
-		float[][] data = CVSReader.read("..\\data\\student_exam_scores.csv");
+
+		double[][] data = CVSReader.read("..\\data\\StudentExamScores.csv");
 		System.out.println("Rows: " + data.length);
 		System.out.println("Colums: " + data[0].length);
+
+		double[][] a = {
+			{1, 2},
+			{3, 4}
+		};
+
+		double[][] b = {
+			{2, 0},
+			{1, 2}
+		};
+
+		try {
+			double[][] c = MatrixOperations.multiplyMatrix(a, b);
+			MatrixOperations.soutMatrix(c);
+		} catch (Exception e) {System.out.println(e.getMessage());}
+
+		double[][] aInv = MatrixOperations.inverse(a);
+		MatrixOperations.soutMatrix(aInv);
 	}
 }
--- src/utils/CVSReader.java
+++ src/utils/CVSReader.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 import java.io.File;
 
 public abstract class CVSReader {
-	public static float[][] read(String file_Path) {
-		ArrayList<float[]> temp_matrix = new ArrayList<>();
+	/**
+	 * This method receives a path for a CVS file, parses it and returns a double matrix.
+	 * @param filePath Relative or Absolute path for CVS.
+	 * @return {@code double[][]} of CVS data.
+	 */
+	public static double[][] read(String filePath) {
+		ArrayList<double[]> tempMatrix = new ArrayList<>();
 		
-		temp_matrix = parse_Rows(file_Path, temp_matrix);
+		tempMatrix = parseRows(filePath, tempMatrix);
 
-		float[][] matrix = new float[temp_matrix.size()][];
+		double[][] matrix = new double[tempMatrix.size()][];
 		for (int i = 0; i < matrix.length; i++)
-			matrix[i] = temp_matrix.get(i);
+			matrix[i] = tempMatrix.get(i);
 
 		return matrix;
 	}
 
-	private static ArrayList<float[]> parse_Rows(String file_Path, ArrayList<float[]> temp_matrix) {
+	private static ArrayList<double[]> parseRows(String file_Path, ArrayList<double[]> temp_matrix) {
 		try {
 			Scanner reader = new Scanner(new File(file_Path));
 
@@ -25,11 +30,11 @@ public abstract class CVSReader {
 
 			while (reader.hasNextLine()) {
 				String[] values = reader.nextLine().split(",");
-				float[] parsed_values = new float[values.length];
+				double[] parsed_values = new double[values.length];
 
 				// Here we attempt to get the real value of each number.
 				for (int i = 0; i < values.length; i++)
-					parsed_values[i] = Float.parseFloat(values[i]);
+					parsed_values[i] = Double.parseDouble(values[i]);
 
 				// We add the row into the matrix.
 				temp_matrix.add(parsed_values);
